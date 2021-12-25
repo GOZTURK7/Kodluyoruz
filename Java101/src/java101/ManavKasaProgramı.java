677eb9eaabb011b740d482b5cf22fac3b5d4123b
Gökhan Öztürk <gokhan.89.ozturk@gmail.com>
2021-12-25 16:19:49 +0300

    ManavKasaProgramı

---
+++ Java101/src/java101/ManavKasaProgramı.java
@@ -0,0 +1,29 @@
+package java101;
+
+import java.util.Scanner;
+
+public class ManavKasaProgramı {
+
+    public static void main(String[] args) {
+
+        double armut=2.14, elma=3.67, domates=1.11, muz=0.95, patlican=5;
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Armut Kaç kilo");
+        int arm = scan.nextInt();
+        System.out.println("elma Kaç kilo");
+        int el = scan.nextInt();
+        System.out.println("domates Kaç kilo");
+        int dom = scan.nextInt();
+        System.out.println("muz Kaç kilo");
+        int mu = scan.nextInt();
+        System.out.println("patlıcan Kaç kilo");
+        int pat = scan.nextInt();
+
+        double toplamTutar= arm*armut + el*elma + dom* domates + mu*muz + pat* patlican;
+
+        System.out.println("Toplam Tutar: "+ toplamTutar);
+
+
+    }
+}
