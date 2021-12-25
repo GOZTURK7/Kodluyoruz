66786bb964466468e88454aa7159f1e466782a4a
Gökhan Öztürk <gokhan.89.ozturk@gmail.com>
2021-12-25 21:20:19 +0300

    ArtıkYıl

---
+++ Java101/src/java101/ArtikYil.java
@@ -0,0 +1,29 @@
+package java101;
+
+import java.util.Scanner;
+
+
+public class ArtikYil {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Lütfen yılı girin:");
+        int yil = scan.nextInt();
+
+        if(yil%4==0){
+            if(yil%100==0){
+                if (yil%400==0){
+                    System.out.println(yil + " Artık yıldır.");
+                }else{
+                    System.out.println(yil +" Artık yıl değildir");
+                }
+            }else{
+                System.out.println(yil+" Artık yıldır.");
+            }
+        }else{
+            System.out.println(yil + " Artik yıl değildir");
+        }
+
+    }
+}
