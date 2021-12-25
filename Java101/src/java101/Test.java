84a0f38331414bb3389d01f80167f75c39f30919
Gökhan Öztürk <gokhan.89.ozturk@gmail.com>
2021-12-25 14:20:13 +0300

    fourth commit

--- Java101/src/java101/Test.java
+++ Java101/src/java101/Test.java
@@ -1,10 +1,9 @@
-package kodluyoruzJava101;
+package java101;
 
 public class Test {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
     }
-
 }
