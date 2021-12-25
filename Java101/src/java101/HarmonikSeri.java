682107229d15903a056d648e37ffe1e7bd1a70bf
Gökhan Öztürk <gokhan.89.ozturk@gmail.com>
2021-12-25 22:55:26 +0300

    Harmonik Seri

---
+++ Java101/src/java101/HarmonikSeri.java
@@ -0,0 +1,22 @@
+package java101;
+
+import java.util.Scanner;
+
+public class HarmonikSeri {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner (System.in);
+
+        System.out.println("Bir sayı girin:");
+
+        int n = scan.nextInt();
+
+        double result=0;
+        for (double i = 1; i <=n; i++) {
+
+            result+=(1/i);
+
+        }System.out.println(result);
+    }
+}
