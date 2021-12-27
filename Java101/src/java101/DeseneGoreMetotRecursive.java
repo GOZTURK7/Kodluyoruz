d155ef4d52133fba440e479cdf7b7668e439f352
Gökhan Öztürk <gokhan.89.ozturk@gmail.com>
2021-12-27 11:41:51 +0300

    DeseneGöreMetot

---
+++ Java101/src/java101/DeseneGoreMetotRecursive.java
@@ -0,0 +1,25 @@
+package java101;
+
+import java.util.Scanner;
+public class DeseneGoreMetotRecursive {
+    static int num;
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner (System.in);
+        System.out.println("Lütfen Bir sayı giriniz:");
+        num = scan.nextInt();
+
+
+        System.out.println(desen(num, 5));
+    }
+
+    private static String desen(int sayi, int i) {
+        if(sayi<=0) i*=(-1);
+        int temp = sayi-i;
+        if(temp==num) return temp+"";
+        return sayi + " " +desen(temp, i);
+
+
+    }
+}
