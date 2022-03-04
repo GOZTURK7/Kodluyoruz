35f9a55383773bdf7cca01d063726804e315b0f8
Gökhan Öztürk <gokhan.89.ozturk@gmail.com>
2022-03-04 18:07:30 +0300

    FiksturOluşturucu

--- Java102/src/fiksturOlusturucu/FiksturOlusturucu.java
+++ Java102/src/fiksturOlusturucu/FiksturOlusturucu.java
@@ -4,9 +4,15 @@ import java.util.*;
 
 public class FiksturOlusturucu {
 
+    List<String> teams = new ArrayList<>();
+    List<Fikstur> fiks = new ArrayList<>();
+    List<Fikstur> fiksturArranged = new ArrayList<>();
+    List<Integer> selectedIndexes = new ArrayList<>();
+
+
 
     public void run(){
-        List<String> teams = new ArrayList<>();
+
 
         Scanner scan = new Scanner (System.in);
         System.out.println("Lütfen Takımları Girin: (çıkmak için 'exit' yazın)");
@@ -16,44 +22,117 @@ public class FiksturOlusturucu {
                 break;
             else teams.add(takım);
         }
-        System.out.println(teams);
+        System.out.println("eklemeden sonra teams: "+ teams);
 
-        if(teams.size() % 2 == 1) teams.add("Bay");
+        generateFixtureList(teams);
+        System.out.println("fiks.size(): "+fiks.size());
 
+//        for (int i = 0; i < fiksturArranged.size(); i+=teams.size()/2)
 
-        FiksturOlusturucu fix = new FiksturOlusturucu();
+        for(Fikstur w : fiks){
 
-        for(Fikstur w : fix.generateFixture(teams)){
-//            System.out.println("Round "+round);
+            System.out.println(w.getHome() + " vs "+ w.getAway());
+
+        }
+
+        System.out.println("******************************");
+
+        for(Fikstur w : fiksturArranged){
+
             System.out.println(w.getHome() + " vs "+ w.getAway());
 
         }
     }
 
 
-    public List<Fikstur> fiks = new ArrayList<>();
 
-    public List<Fikstur> generateFixture(List<String> teams){
 
-        int round = ((teams.size()-1)*2);
+    public void generateFixtureList(List<String> teams) {
+
+        if (teams.size() % 2 == 1) teams.add("Bay");
+        int round = ((teams.size() - 1) * 2);
         int match = teams.size();
 
-        for(int i=0; i<match; i++){
+        for (int i = 0; i < match; i++) {
 
-            for(int j=0; j<match; j++){
+            for (int j = 0; j < match; j++) {
 
-                if(teams.get(i).equals(teams.get(j))){
+                if (teams.get(i).equals(teams.get(j))) {
                     continue;
-                }else{
+                } else {
                     fiks.add(new Fikstur(teams.get(i), teams.get(j)));
                 }
 
             }
 
         }
-        Collections.shuffle(fiks);
 
-        return fiks;
+
+        for (int i = 0; i < fiks.size(); i++) {
+
+            for (int j = 0; j < fiks.size(); j++) {
+
+                fiksturArranged.add(fiks.get(i));
+
+                if(i==j) continue;
+
+                if(!fiksturArranged.get(i).getHome().equals(fiks.get(j).getAway())){
+                    fiksturArranged.add(fiks.get(j));
+                     break;
+
+                }
+            }
+
+
+        }
+
+
+    }
+
+    public void generateFixture(List<Fikstur> fiks, List<String> teams){
+
+        int start = 0;
+        int finish=teams.size()-1; // 5
+
+        while(true){
+            int index = getRandomIndex(start,finish);
+            if(selectedIndexes.contains(index)){
+                continue;
+            }
+            selectedIndexes.add(index);
+            fiksturArranged.add(fiks.get(index));
+            if(start==25){
+                start=0;
+                finish=teams.size()-1; // 5
+            }
+            start =finish;
+            finish= start + teams.size()-1;
+
+            if(selectedIndexes.size()==fiks.size()){
+                break;
+            }
+        }
+
+
+
+//        Collections.shuffle(fiks);
+
+    }
+
+    public int getRandomIndex(int min, int max){
+        Random rand = new Random();
+        int a = rand.nextInt(max-min);
+        while(true){
+            if(selectedIndexes.contains(a+min)){
+
+                a=rand.nextInt(max-min);
+
+            }else{
+                break;
+            }
+        }
+
+        return a+min;
 
     }
 
